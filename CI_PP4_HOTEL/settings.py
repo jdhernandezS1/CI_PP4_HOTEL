1baf7c66cbe7cbf0a1a7fc6919b5a52f1a3eb6f4
David <julio-david@hotmail.com>
2022-08-28 16:07:42 +0200

    Heroku deployment

--- CI_PP4_HOTEL/settings.py
+++ CI_PP4_HOTEL/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +25,7 @@ SECRET_KEY = 'django-insecure-+u7qwe1n(^f4bgi*vh%j(pd+*)r7jttlri%x=7lc7#=ve-+cbv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,10 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS= (os.path.join(BASE_DIR, 'static'),)
-django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn cipp4hotel.wsgi
+web: gunicorn myproject.wsgi
