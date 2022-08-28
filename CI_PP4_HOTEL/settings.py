15d395025c960db45260d5188b397acce2fb0431
David <julio-david@hotmail.com>
2022-08-28 16:29:54 +0200

    fixing deployment

--- CI_PP4_HOTEL/settings.py
+++ CI_PP4_HOTEL/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+u7qwe1n(^f4bgi*vh%j(pd+*)r7jttlri%x=7lc7#=ve-+cbv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
